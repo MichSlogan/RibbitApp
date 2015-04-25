6fbe4c2ae6418730d262066ef1cc6067590222ce
Pierre Larose <XCODE@pierres-air.home>
2015-04-25 09:21:32 -0400

    Initial Commit

---
+++ Ribbit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ribbit
+//
+//  Created by Pierre Larose on 4/25/15.
+//  Copyright (c) 2015 Pierre Larose. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
